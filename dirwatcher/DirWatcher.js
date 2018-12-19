b3354d26794f1d21f1199bf80f040b8416154707
Lasitsa, Uladzislau <Uladzislau.Lasitsa@kronos.com>
2018-12-19 18:26:07 +0300

    DirWatcher refactoring

--- dirwatcher/DirWatcher.js
+++ dirwatcher/DirWatcher.js
@@ -9,36 +9,42 @@ class DirWatcher {
 
     watch(sPath, iDelay) {
         let oldData = {};
-        setTimeout(function checkFiles() {
+        setTimeout(function checkDirChanges() {
             fs.readdir(sPath, (oErr, aFiles) => {
                 if (!oErr) {
-                    let aDeletedFiles = Object.keys(oldData).filter(sFileName => {
-                        if (aFiles.indexOf(sFileName) === -1) {
-                            delete oldData[sFileName];
-                            return true;
-                        }
-                    });
-                    aDeletedFiles.forEach(sDelitedFileName => console.log(`File ${sDelitedFileName} was removed!`));
-                    aFiles.forEach(sFileName => {
-                        let sFilePath = sPath + "/" + sFileName;
-                        fs.stat(sFilePath, (oErr, oData) => {
-                            if (oErr) {
-                                console.log(oErr);
-                            } else {
-                                if (!oldData[sFileName] || oldData[sFileName] !== oData.mtimeMs) {
-                                    console.log(`File ${sFileName} was changed!`);
-                                    this.emitter.emit("dirwatcher:changed", sFilePath);
-                                    oldData[sFileName] = oData.mtimeMs;
-                                }
-                            }
-                        });
-                    });
+                    this.checkDeletedFiles(oldData, aFiles);
+                    aFiles.forEach(sFileName => this.checkFileChanged(sPath, sFileName, oldData));
                 } else {
                     console.log(oErr);
                 }
             });
-            setTimeout(checkFiles.bind(this), iDelay);
+            setTimeout(checkDirChanges.bind(this), iDelay);
         }.bind(this), iDelay);
+    }
+
+    checkDeletedFiles(oldData, aFiles) {
+        let aDeletedFiles = Object.keys(oldData).filter(sFileName => {
+            if (aFiles.indexOf(sFileName) === -1) {
+                delete oldData[sFileName];
+                return true;
+            }
+        });
+        aDeletedFiles.forEach(sDelitedFileName => console.log(`File ${sDelitedFileName} was removed!`));
+    }
+
+    checkFileChanged(sPath, sFileName, oldData) {
+        let sFilePath = sPath + "/" + sFileName;
+        fs.stat(sFilePath, (oErr, oData) => {
+            if (oErr) {
+                console.log(oErr);
+            } else {
+                if (!oldData[sFileName] || oldData[sFileName] !== oData.mtimeMs) {
+                    console.log(`File ${sFileName} was changed!`);
+                    this.emitter.emit("dirwatcher:changed", sFilePath);
+                    oldData[sFileName] = oData.mtimeMs;
+                }
+            }
+        });
     }
 }
 
