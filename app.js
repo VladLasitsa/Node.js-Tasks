5b0c3784772236dcdbd2001783f6e8670f87feac
Lasitsa, Uladzislau <Uladzislau.Lasitsa@kronos.com>
2019-03-29 12:20:48 +0300

    Node.js task - 8 swagger

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import expressSession from 'express-session';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import yamljs from 'yamljs';
 import routes from './routes/routes';
 import {cookieParser} from './middlewares/cookie-parser';
 import {queryParser} from './middlewares/query-parser';
@@ -9,6 +11,7 @@ import {jwtTokenVerify} from './middlewares/jwtToken-verify';
 import passport from './utils/authenticationStrategiesUtil';
 
 const app = express();
+const swaggerDoc = yamljs.load('./swagger.yaml');
 
 app.use(queryParser);
 app.use(cookieParser);
@@ -18,5 +21,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 //app.use(jwtTokenVerify);
 app.use('/', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 export default app;
